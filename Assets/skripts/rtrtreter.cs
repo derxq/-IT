be3e1aabe8d956bfc63bc03ef9b6535034d18dfc
321123e <122577449+321123e@users.noreply.github.com>
2023-02-14 10:24:24 +1000

    интро работает

--- Assets/skripts/rtrtreter.cs
+++ Assets/skripts/rtrtreter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextG : MonoBehaviour
+public class rtrtreter : MonoBehaviour
 {
     public Text TextGameObject;
     private string text;
