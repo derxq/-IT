e8c47b09b45e41676e4e24633930bdcc7ad4a3fd
derxq <arsenbir09@gmail.com>
2023-02-14 10:19:25 +1000

    ff

---
+++ Assets/skripts/IButton.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.skripts
+{
+    public interface IButton
+    {
+        void ButtorEXIT();
+        void ButtorPlay();
+        bool Equals(object other);
+        int GetHashCode();
+        string ToString();
+    }
+}
