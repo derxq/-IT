a95c121e0e3cc185140c48ef31d83e371ffb7399
321123e <122577449+321123e@users.noreply.github.com>
2023-05-02 10:30:43 +1000

    доработка 2 уровня

--- Assets/skripts/F2ndLevel.cs
+++ Assets/skripts/F2ndLevel.cs
@@ -7,18 +7,18 @@ public class F2ndLevel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // запускаем звук крика
-        AudioClip hitSound;
+        // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+        //AudioClip hitSound;
     }
 
     void OnCollisionEnter(Collision Col)
     {
-        if (Col.gameObject.name == "Ground")
-        {
+        //if (Col.gameObject.name == "Ground")
+        //{
 
-            gameObject.audio.clip = hitSound;
-        }
-            // запускаем звук ломающихся досок
+            //gameObject.GetComponent<AudioSource>().clip = hitSound;
+        //}
+            // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
 
 
 
--- Assets/skripts/шипы.cs
+++ Assets/skripts/шипы.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class шипы : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        if (other.tag == "Player")
+        SceneManager.LoadScene("DefeatMenu");
 
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
+     }
 }
